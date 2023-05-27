df421e0f2663557f5466fa2806ba2ae3c8bdfeaa
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-05-27 10:26:11 +0300

    Add documentations for all endpoints

--- GeoDB_Web_Backend_Development/settings.py
+++ GeoDB_Web_Backend_Development/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.gis",
     "rest_framework",
+    "drf_spectacular",
     "debug_toolbar",
     "location",
     "user",
@@ -166,7 +167,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
--- location/views.py
+++ location/views.py
@@ -1,25 +1,80 @@
-from datetime import datetime
-
-from django.db.models import F, Count
-from drf_spectacular.types import OpenApiTypes
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from rest_framework import viewsets
 from drf_spectacular.utils import extend_schema, OpenApiParameter
-from rest_framework import viewsets, mixins, status
-from rest_framework.decorators import action
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
-
-from location.models import Place
-from location.serializers import PlaceSerializer
+from .models import Place
+from .serializers import PlaceSerializer
 
 
-# Create your views here.
-class PlaceViewSet(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    GenericViewSet,
-):
+class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
     permission_classes = (IsAuthenticated,)
+
+    @extend_schema(
+        methods=["POST"],
+        summary="Create a new place",
+        description="This endpoint creates a new place. (name, description, geom(POINT)",
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="List of all Places",
+        parameters=[
+            OpenApiParameter(
+                "latitude",
+                type=str,
+                description="Specify the latitude parameters in the format like 1.000 (floating-point number)",
+            ),
+            OpenApiParameter(
+                "longitude",
+                type=str,
+                description="Specify the longitude parameters in the format like 1.000 (floating-point number)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs) -> Response:
+        """
+        Here you can view a list of all coordinates available in the database.
+        Using the provided coordinates, you will be able to find the nearest point.
+        """
+        latitude = float(request.query_params.get("latitude"))
+        longitude = float(request.query_params.get("longitude"))
+
+        point = Point(longitude, latitude, srid=4326)
+
+        nearest_place = Place.objects.annotate(
+            distance=Distance("geom", point)
+        ).order_by('distance').first()
+
+        serializer = self.get_serializer(nearest_place)
+        return Response(serializer.data)
+
+    @extend_schema(
+        methods=["GET"],
+        summary="Retrieve a place by ID",
+        description="This endpoint retrieves a specific place by its ID.",
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        methods=["PUT"],
+        summary="Update a place",
+        description="This endpoint update a specific place. (name = string, description = text, geom = POINT(1.000, 1.000).",
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        methods=['DELETE'],
+        summary="Delete a place",
+        description="This endpoint deletes a specific place by ID.",
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+
